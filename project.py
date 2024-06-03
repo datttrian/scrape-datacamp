2ffe5c6e1ef01e029c51c72c666ff481c11195c8
datttrian <datttrian@gmail.com>
2024-06-03 08:51:57 +0000

    Mon, Jun 3, 2024, 8:51 AM +00:00

--- project.py
+++ project.py
@@ -1,5 +1,7 @@
 import sys
 from langchain_community.document_loaders import DirectoryLoader
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 DATA_PATH = "data/books"
@@ -7,7 +9,8 @@ DATA_PATH = "data/books"
 
 def main():
     documents = load_documents()
-    print(documents)
+    chunks = split_text(documents)
+    print(chunks)
 
 
 def load_documents():
@@ -18,6 +21,23 @@ def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob=pdf_file)
     documents = loader.load()
     return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
 
 
 if __name__ == "__main__":
