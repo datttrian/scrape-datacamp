76eec525aae74a8aa3a4ed439ca82025b3aea5c6
datttrian <datttrian@gmail.com>
2024-06-03 09:34:22 +0000

    Mon, Jun 3, 2024, 9:34 AM +00:00

--- project.py
+++ project.py
@@ -25,7 +25,9 @@ def main():
     embedding_function = OpenAIEmbeddings()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
     results = db.similarity_search_with_relevance_scores(query_text, k=3)
-
+    if len(results) == 0 or results[0][1] < 0.7:
+        print("Unable to find matching results.")
+        return
     print(results)
 
 
