19e17a5893e4b010d0079d7516b0f4ab828d75f1
datttrian <datttrian@gmail.com>
2024-06-03 09:52:37 +0000

    Mon, Jun 3, 2024, 9:52 AM +00:00

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
 
 CHROMA_PATH = "chroma"
@@ -18,7 +18,7 @@ You are an intelligent assistant. You have been provided with the following cont
 
 Based on this context, please provide a detailed and informative answer to the following question:
 
-Question: {question}
+{question}
 """
 
 
@@ -36,7 +36,7 @@ def main():
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
     results = db.similarity_search_with_relevance_scores(query_text, k=3)
     if len(results) == 0 or results[0][1] < 0.7:
-        print("Unable to find matching results.")
+        print("Please ask a question related to the PDF file!")
         return
     print(results)
 
@@ -44,6 +44,13 @@ def main():
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
+
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
 
 
 def load_documents(file):
