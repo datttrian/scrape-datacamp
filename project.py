3f40ea51461fde01d0df14a37fc941323a587d73
datttrian <datttrian@gmail.com>
2024-06-04 10:08:22 +0000

    Tue, Jun 4, 2024, 10:08 AM +00:00

--- project.py
+++ project.py
@@ -53,6 +53,16 @@ def split_text(documents: list[Document]):
     return chunks
 
 
+# def save_to_chroma(chunks: list[Document]):
+#     if os.path.exists(CHROMA_PATH):
+#         shutil.rmtree(CHROMA_PATH)
+
+#     db = Chroma.from_documents(
+#         chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+#     )
+#     db.persist()
+#     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
 def save_to_chroma(chunks: list[Document]):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
