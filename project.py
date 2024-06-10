30944e3b4ff2f8c81a9d6be8ccc2423bde1f8114
datttrian <datttrian@gmail.com>
2024-06-10 09:27:16 +0000

    Mon, Jun 10, 2024, 9:27 AM +00:00

--- project.py
+++ project.py
@@ -1,8 +1,10 @@
 import os
+import time
 
 import openai
 from dotenv import load_dotenv
 from openai import OpenAI
+from PIL import Image
 
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -22,15 +24,39 @@ assistant = client.beta.assistants.create(
     tool_resources={"code_interpreter": {"file_ids": [file.id]}},
 )
 
-
 thread = client.beta.threads.create(
     messages=[
         {
             "role": "user",
-            "content": "Create 3 data visualizations based on the trends in this file.",
+            "content": "Create 3 data visualizations using two columns or more based on the trends in this file.",
             "attachments": [
                 {"file_id": file.id, "tools": [{"type": "code_interpreter"}]}
             ],
         }
     ]
 )
+
+run = client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id)
+print(f"👉 Run Created: {run.id}")
+
+while run.status != "completed":
+    run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+    print(f"🏃 Run Status: {run.status}")
+    time.sleep(1)
+else:
+    print("🏁 Run Completed!")
+
+message_response = client.beta.threads.messages.list(thread_id=thread.id)
+
+
+image_data = client.files.content(
+    message_response.data[0].content[0].image_file.file_id
+)
+image_data_bytes = image_data.read()
+
+with open("my-image.png", "wb") as file:
+    file.write(image_data_bytes)
+
+
+image = Image.open("my-image.png")
+image.show()
